b98f6a64902d49432dc58de706234d811a810478
rajiarazz <85434007+rajiarazz@users.noreply.github.com>
2021-06-07 20:26:15 +0530

    Add files via upload

---
+++ task1/task1.py
@@ -0,0 +1,13 @@
+#write a program that uses input to prompt a user for their name and then welcomes them.
+name= input('your good name, please.')
+print ('Hello'),name
+#Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+celsius=input("enter the celsius temperature:")
+celcius=int(celsius)
+fahrenheit=((celcius*(9/5)+32));
+print("Fahrenheit:"+str(fahrenheit));
+#Write a Python program to convert degree to radian.
+degree=input("enter the degree value:")
+degree=int(degree)
+radian=(degree*3.14/180);
+print("radian:"+str(radian));
